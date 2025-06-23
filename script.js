c86d08f8dd27e005bd2b65ec4daec8e59c8d948f
kartavyashishangiya1409 <kartavyashishangiya1409@gmail.com>
2025-06-23 17:32:00 +0530

    Corrected user-input section

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let search = document.querySelector(".search")
 
-function autoResize() {
-  search.style.height = "auto";
-  search.style.height = (search.scrollHeight) + "px";
+function autoResize(el) {
+  el.style.height = "auto";
+  el.style.height = el.scrollHeight + "px";
 }
